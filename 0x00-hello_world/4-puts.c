afb25a7f1772b4f85b84ac470202f9e5599284a4
kaphla <khaynaal@gmail.com>
2023-02-20 16:27:42 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is a program that prints a new line with a puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
