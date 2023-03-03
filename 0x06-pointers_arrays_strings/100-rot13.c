3edebcdc6f87206ca20f0b581833b55aca1cbebf
kaphla <khaynaal@gmail.com>
2023-03-02 16:12:48 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * rot13 - A function that encodes a string using rot13
+ * @str: The strings
+ * Return: 0
+ */
+
+char *rot13(char *str)
+{
+	int den1 = 0;
+	int den2;
+
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+			'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm', 'n', 'o',
+			'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+	while (str[den1])
+	{
+		for (den2 = 0; den2 < 52; den2++)
+		{
+			if (str[den1] == alphabet[den2])
+			{
+				str[den1] = rot13key[den2];
+				break;
+			}
+	}
+	den1++;
+}
+return (str);
+}
